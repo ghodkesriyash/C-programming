da0293bd06a06ba5f4195343d82bb4f20bddf42f
Sriyash Ghodke <ghodkesriyash@gmail.com>
2026-03-20 05:59:00 +0000

    modify

---
+++ Assignments/Assignment15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main()
+{
+    int n;
+    printf("Enter how many random numbers you want: ");
+    scanf("%d", &n);
+
+    srand(time(NULL));  // seed with current time
+
+    printf("Pseudo Random Numbers:\n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d\n", rand());
+    }
+}
